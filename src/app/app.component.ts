12f1c6abad8e741cf874f65883aeb9ee35ab5873
M.Mavromanolakis <mauromanolakism@gmail.com>
2024-11-11 16:34:30 +0200

    component input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
+import { Person } from './shared/interfaces/person';
 
 
 @Component({
@@ -12,5 +13,19 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 export class AppComponent {
   name = 'Michalis';
 
-  
+  person0: Person = {
+      givenName: 'Michalis',
+      surName: 'Mavromanolakis',
+      age: 36,
+      email: 'mauromanolakism@gmail.com',
+      address: 'Stratou 23'
+    };
+
+    person1: Person = {
+      givenName: 'Bob',
+      surName: 'Marley',
+      age: 76,
+      email: 'Bob @gmail.com',
+      address: 'kerkis 113'
+    };
 }
