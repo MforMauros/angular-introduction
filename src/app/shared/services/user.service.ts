bca4e5ef534d92c43be35c09af6c2d6ab211bd38
M.Mavromanolakis <mauromanolakism@gmail.com>
2025-01-13 12:11:08 +0200

    login user finished

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,16 +1,42 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { effect, inject, Injectable, signal } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { User } from '../interfaces/user';
+import { Credentials, LoggedInUser, User } from '../interfaces/user';
+import { Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
 
-const API_URL = `${environment.apiURL}/user`
+const API_URL = `${environment.apiURL}/user`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
   http: HttpClient = inject(HttpClient);
+  router: Router = inject(Router);
+
+  user = signal<LoggedInUser | null>(null);
+
+  
+
+  constructor() {
+    effect(() => {
+      const access_token = localStorage.getItem('access_token');
+      
+      if (access_token) {
+        const decodedTokenSubject = jwtDecode(access_token)
+          .sub as unknown as LoggedInUser;
+        this.user.set({
+          fullname: decodedTokenSubject.fullname,
+          email: decodedTokenSubject.email,
+        });
+      }
+      if (this.user()) {
+        console.log('User logged in', this.user().fullname);
+      } else {
+        console.log('No user logged in');
+      }
+    });
+  }
 
   registerUser(user: User) {
     return this.http.post<{msg:string}>(`${API_URL}/register`, user);
@@ -18,5 +44,15 @@ export class UserService {
 
   check_dublicate_email(email: string) {
     return this.http.get<{msg:string}>(`${API_URL}/check_dublicate_email${email}`);
+  }
+
+  loginUser(credentials: Credentials) {
+    return this.http.post<{access_token: string}>(`${API_URL}/login`, credentials)
+  }
+
+  logoutUser() {
+    this.user.set(null);
+    localStorage.removeItem('access_token');
+    this.router.navigate(['login']);
   }
 }
